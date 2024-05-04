cd63d0acbf873221156196b93c1dd03fcfd28e2b
Thomas Steinert <hello@chroni.cc>
2024-05-04 11:59:54 +0200

    refactor: split config calls from value assignement for easy affirmation which stack options exist

--- pulumi/apps/s3ninja.py
+++ pulumi/apps/s3ninja.py
@@ -6,11 +6,15 @@ class S3Ninja(K8sDeployment):
 
     def configure(self, name: str):
         config = pulumi.Config(name)
+
+        image_version = config.get("imageVersion", "latest")
+        ingress_host = config.require("ingressHost")
+
         self.image_name = "chronicc/s3ninja"
-        self.image_version = config.get("imageVersion", "latest")
+        self.image_version = image_version
         self.ingress_annotations = {
             "nginx.ingress.kubernetes.io/app-root": "/ui",
         }
         self.ingress_enabled = True
-        self.ingress_host = config.require("ingressHost")
+        self.ingress_host = ingress_host
         self.service_target_port = 9000
